90ab24f7eaafdeef6c3658127ffc5a1594d7c561
yuanda <34027873+yuandahanif@users.noreply.github.com>
2022-07-04 07:58:25 +0700

    feat: :sparkles: add react-table and try chartjs2

--- src/pages/dashboard/admin/index.tsx
+++ src/pages/dashboard/admin/index.tsx
@@ -1,13 +1,52 @@
+import type { ChartData } from 'chart.js';
+import { ArcElement, Chart as ChartJS, Legend, Tooltip } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
 import { NextPageWithLayoutAndAuth } from 'types/custom_next_page';
 
 import AdminLayout from '@/components/layout/Admin.layout';
 import Seo from '@/components/Seo';
 
 const AdminDashboardIndex: NextPageWithLayoutAndAuth = () => {
+  ChartJS.register(ArcElement, Tooltip, Legend);
+
+  const data: ChartData<'pie'> = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+        ],
+        // borderColor: [
+        //   'rgba(255, 99, 132, 1)',
+        //   'rgba(54, 162, 235, 1)',
+        //   'rgba(255, 206, 86, 1)',
+        //   'rgba(75, 192, 192, 1)',
+        //   'rgba(153, 102, 255, 1)',
+        //   'rgba(255, 159, 64, 1)',
+        // ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
   return (
     <>
       <Seo templateTitle='Admin Beranda' />
-      <div>this is sample page</div>
+      <div className='flex gap-x-10'>
+        <div className='w-full max-w-md bg-red-200'>
+          <Pie data={data} />
+        </div>
+        <div className='w-full bg-red-200'>
+          <Pie data={data} />
+        </div>
+      </div>
     </>
   );
 };
